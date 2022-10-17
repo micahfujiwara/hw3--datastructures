59effe54f13d7a5226b59e5d3c518a1902935ce4
micahfujiwara <mdf04414@creighton.edu>
2022-10-17 16:24:31 -0500

    test2

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,7 +1,13 @@
 import java.util.Collections;
 import java.util.List;
 
+/*
+ * java docs here
+ */
+
 public class Algorithms {
+
+    //add the numbers 0 to n from smallest to largest
     public static void addInOrder(List<Integer> numbers, int n){
         long startTime = System.currentTimeMillis();
 
@@ -13,16 +19,18 @@ public class Algorithms {
 
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
+        System.out.println("for list size of " + n + ", the duration is " + duration + " msec");
     }
 
 
+    //add the numbers 0 to n in order from largest to smallest
     public static void addInReverseOrder(List<Integer> numbers, int n){
         for (int i = 0; i < n; i++){
             
         }
     }
 
-
+    //add n random numbers (use all possible int values)
     public static void addInRandomOrder(List<Integer> numbers, int n){
         for (int i = 0; i < n; i++){
             
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+
+/*
+ * java docs here
+ */
+
 public class Main {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        ArrayList<Integer> alist = new ArrayList<Integer>();
+        LinkedList<Integer> llist = new LinkedList<Integer>();
         Algorithms obj = new Algorithms();
 
         System.out.println("Enter the Initial list size: ");
@@ -18,6 +23,53 @@ public class Main {
         int round5 = round4 * 2;
 
 
+        //arraylist addInOrder
+        obj.addInOrder(alist, round1);
+        obj.addInOrder(alist, round2);
+        obj.addInOrder(alist, round3);
+        obj.addInOrder(alist, round4);
+        obj.addInOrder(alist, round5);
+
+        
+        /*
+        //arraylist addInReverseOrder
+        obj.addInReverseOrder(alist, round1);
+        obj.addInReverseOrder(alist, round2);
+        obj.addInReverseOrder(alist, round3);
+        obj.addInReverseOrder(alist, round4);
+        obj.addInReverseOrder(alist, round5);
+        
+        //arraylist addInRandomOrder
+        obj.addInRandomOrder(alist, round1);
+        obj.addInRandomOrder(alist, round2);
+        obj.addInRandomOrder(alist, round3);
+        obj.addInRandomOrder(alist, round4);
+        obj.addInRandomOrder(alist, round5);
+
+*/ 
+
+        //linkedlist addInOrder
+        obj.addInOrder(llist, round1);
+        obj.addInOrder(llist, round2);
+        obj.addInOrder(llist, round3);
+        obj.addInOrder(llist, round4);
+        obj.addInOrder(llist, round5);
+
+    /* 
+        //linkedlist addInReverseOrder
+        obj.addInReverseOrder(llist, round1);
+        obj.addInReverseOrder(llist, round2);
+        obj.addInReverseOrder(llist, round3);
+        obj.addInReverseOrder(llist, round4);
+        obj.addInReverseOrder(llist, round5);
+
+        //linkedlist addInRandomOrder
+        obj.addInRandomOrder(llist, round1);
+        obj.addInRandomOrder(llist, round2);
+        obj.addInRandomOrder(llist, round3);
+        obj.addInRandomOrder(llist, round4);
+        obj.addInRandomOrder(llist, round5);
+*/
         in.close();
     }
 }
